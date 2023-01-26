52d9a62d639ab68180f72dc67f78ddb78bf6d5b9
MinecraftMrsC <108143370+MinecraftMrsC@users.noreply.github.com>
2023-01-26 12:03:51 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+scene.setBackgroundColor(9)
+let list = [
+21,
+57,
+74,
+99,
+130,
+189,
+267
+]
+let mySprite = sprites.create(img`
+    ...................cc...
+    ...............cccc63c..
+    ..............c633336c..
+    ..........cc.c6cc33333c.
+    .........b55c6c55c33333c
+    .........ff5c6c5ff33333c
+    .........ff5c6c5ff6333cc
+    .........b553c355c6666cc
+    ..........b55355c333333c
+    .........cc55555bcc3333c
+    ........c5545554b55c33c.
+    ........b54b4444bb5cbb..
+    ........c455b4b5554c45b.
+    ........c555c4c555c4c5c.
+    ........c5555c5555c4c5c.
+    .........ccccccccc..ccc.
+    `, SpriteKind.Player)
+let target = game.askForNumber("What number?", 3)
+let low = 0
+let high = list.length - 1
